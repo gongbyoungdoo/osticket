369215b082ca2153fdfec8e96e5eaadde2b5bf57
gongbyoungdoo <60121068+gongbyoungdoo@users.noreply.github.com>
2020-10-13 16:37:11 +0900

    Update class.ostsession.php

--- include/class.ostsession.php
+++ include/class.ostsession.php
@@ -320,15 +320,6 @@ class FallbackSessionBackend {
         // the session.save_path
     }
 }
-$this->data->session_data = ""; 
-After:
 
-catch (DoesNotExist $e) {
-    $this->data = new SessionData(['session_id' => $id]); 
-So it will be:
-
-catch (DoesNotExist $e) {
-    $this->data = new SessionData(['session_id' => $id]);
-    $this->data->session_data = "";
 
 ?>
